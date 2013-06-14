4891580ac7df8aaa813ad0c29851863814ea9116
Calvin Cheng <linchuan.cheng@gmail.com>
2013-06-15 07:21:26 +0800

    Using Roomsmixin

--- chat/sockets.py
+++ chat/sockets.py
@@ -1,45 +1,12 @@
 import logging
 
 from socketio.namespace import BaseNamespace
-#from socketio.mixins import RoomsMixin, BroadcastMixin
-from socketio.mixins import BroadcastMixin
+from socketio.mixins import RoomsMixin, BroadcastMixin
 from socketio.sdjango import namespace
 
 
-class LonelyRoomMixin(object):
-    def __init__(self, *args, **kwargs):
-        super(LonelyRoomMixin, self).__init__(*args, **kwargs)
-        if 'rooms' not in self.session:
-            self.session['rooms'] = set()  # a set of simple strings
-
-    def join(self, room):
-        """Lets a user join a room on a specific Namespace."""
-        self.session['rooms'].add(self._get_room_name(room))
-
-    def leave(self, room):
-        """Lets a user leave a room on a specific Namespace."""
-        self.session['rooms'].remove(self._get_room_name(room))
-
-    def _get_room_name(self, room):
-        return self.ns_name + '_' + room
-
-    def emit_to_room(self, room, event, *args):
-        """This is sent to all in the room (in this particular Namespace)"""
-        pkt = dict(type="event",
-                   name=event,
-                   args=args,
-                   endpoint=self.ns_name)
-        room_name = self._get_room_name(room)
-        for sessid, socket in self.socket.server.sockets.iteritems():
-            if 'rooms' not in socket.session:
-                continue
-            #if room_name in socket.session['rooms'] and self.socket != socket:
-            if room_name in socket.session['rooms']:
-                socket.send_packet(pkt)
-
-
 @namespace('/chat')
-class ChatNamespace(BaseNamespace, LonelyRoomMixin, BroadcastMixin):
+class ChatNamespace(BaseNamespace, RoomsMixin, BroadcastMixin):
     nicknames = []
 
     def initialize(self):
